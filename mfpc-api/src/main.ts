8007c561e0bac8247cd5c8d0fc402b3d8796b4e3
nesarares <nesa.rares@gmail.com>
2021-01-13 21:00:41 +0200

    Changed server logger

--- mfpc-api/src/main.ts
+++ mfpc-api/src/main.ts
@@ -5,13 +5,28 @@ process.env['NODE_CONFIG_DIR'] = path.join(__dirname, 'config') + path.sep;
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as config from 'config';
+import * as morgan from 'morgan';
+import { TimeUtils } from './shared/utils/time.utils';
+import * as consoleStamp from 'console-stamp';
+import * as chalk from 'chalk';
+
+consoleStamp(console, {
+  format: ':date(yyyy-mm-dd HH:MM:ss).green'
+});
+
+morgan.token('customDate', () => chalk.green(`[${TimeUtils.nowFormatted}]`));
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {
+    logger: false
+  });
   app.setGlobalPrefix('api');
   app.enableCors();
+  app.use(morgan(`:customDate :method ${chalk.yellow(':url')} :status :response-time ms - :res[content-length] B`))
 
   const port = config.get<number>('server.port');
   await app.listen(port);
+
+  console.log(`Server started listening on port ${port}`);
 }
 bootstrap();
---
+++ mfpc-api/src/shared/utils/time.utils.ts
@@ -0,0 +1,7 @@
+import * as dayjs from 'dayjs';
+
+export class TimeUtils {
+	public static get nowFormatted() {
+		return dayjs().format('YYYY-MM-DD HH:mm:ss');
+	}
+}
